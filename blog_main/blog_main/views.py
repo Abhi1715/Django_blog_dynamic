50a3e836f3d6b2c617ec94fc50a34d3cfd28efe9
Abhi1715 <abhilashkurunelli02@gmail.com>
2025-12-10 21:56:37 +0530

    implements template inheritance and url routing

--- blog_main/blog_main/views.py
+++ blog_main/blog_main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from blogs.models import Category,Blog
 
 def home(request):
-    return render(request,'home.html')
+    
+    featured = Blog.objects.filter(is_featured=True,status=1).order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False ,status=1)
+    context={
+        
+        'featured':featured,
+        'posts' : posts,
+    }
+    return render(request,'home.html',context)
--- blog_main/blogs/views.py
+++ blog_main/blogs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from . models import Blog,Category
 
 # Create your views here.
+def posts_by_category(request,category_id):
+    posts=Blog.objects.filter(status=1,category=category_id)
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    context={
+        'posts':posts,
+        'category': category,
+    }
+    return render(request,'posts_by_category.html',context)
